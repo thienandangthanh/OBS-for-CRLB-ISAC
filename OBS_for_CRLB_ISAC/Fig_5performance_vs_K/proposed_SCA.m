53ef3e6920e18963aedebd4387354a2d19f85954
Thien An Dang Thanh <24854119+thienandangthanh@users.noreply.github.com>
2025-05-28 23:02:18 +0900

    refactor(matlab): extract construct_steer_matrix_and_derivative_steer_matrix to utils
    This function is used for UPA steering matrix calculations.
    Reason: the duplication in multiple files is not necessary and not elegant.

--- OBS_for_CRLB_ISAC/Fig_5performance_vs_K/proposed_SCA.m
+++ OBS_for_CRLB_ISAC/Fig_5performance_vs_K/proposed_SCA.m
@@ -306,38 +306,3 @@ end
 
 
 
-
-function [A,dAtheta,dAphi]=construct_steer_matrix_and_derivative_steer_matrix(theta,phi,Mx,My)
-% input:
-% theta - Elevation angle (in radians)
-% phi   - Azimuth angle (in radians)
-% Mx    - Number of elements along x-axis
-% My    - Number of elements along y-axis
-
-% Generate indices for x-axis and y-axis
-ix = (0:Mx-1)'; % Column vector for x-axis indices
-iy = (0:My-1)'; % Column vector for y-axis indices
-
-% Number of steering vectors
-M=length(theta);
-% Preallocate outputs
-A=zeros(Mx*My,M);
-dAtheta=zeros(Mx*My,M);
-dAphi=zeros(Mx*My,M);
-% Compute steering vectors along x and y axes
-for m=1:M
-    % Steering vectors for x and y dimensions
-    ax =1/sqrt(Mx)* exp(1j * pi * ix * sin(theta(m)) * sin(phi(m)));
-    ay =1/sqrt(My)* exp(1j * pi * iy * cos(phi(m)));
-    % Derivatives of ax and ay
-    daxtheta=1j*pi*ix*cos(theta(m))*sin(phi(m)).*ax;
-
-    daxphi=1j*pi*ix*sin(theta(m))*cos(phi(m)).*ax;
-    dayphi=-1j*pi*iy*sin(phi(m)).*ay;
-    % Compute the full UPA steering vector using Kronecker product
-    A(:,m) = kron(ay, ax); % Kronecker product combines ax and ay
-    dAtheta(:,m)=kron(ay,daxtheta);
-    dAphi(:,m)=kron(ay,daxphi)+kron(dayphi,ax);
-end
-
-end
